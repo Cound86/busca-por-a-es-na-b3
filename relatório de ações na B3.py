be09e443a16037cee24250adbb33b2741192e8e6
Rose Santos <rosesantos6891@yahoo.com>
2023-04-25 17:16:08 -0300

    commit tres

---
+++ relatório de ações na B3.py
@@ -0,0 +1,28 @@
+import numpy
+import pandas
+from pandas_datareader import data as web
+import yfinance
+
+yfinance.pdr_override()
+
+ABEV3 = web.get_data_yahoo("ABEV3.SA", start='2020-3-3')
+
+ABEV3.info()
+
+ABEV3.head()
+
+ABEV3.tail()
+
+
+carteira_de_investimentos  = ['ABEV3.SA', 'ASAI3.SA', 'CRFB3.SA']
+novos_dados = pandas.DataFrame()
+for cdi in carteira_de_investimentos:
+    novos_dados[cdi] = web.DataReader(cdi, data_source='yahoo', start='2020-3-3')['Adj Close']
+    
+    novos_dados.tail()
+    
+novos_dados.to_csv(
+    '/home/rose/portifólio/Faz relatórios de ações de empresas na B3/dados importados do doc em ods.csv')
+novos_dados1 = pd.read_csv(
+    '/home/rose/dados importados do doc em ods.csv')
+
