223ff11295b72ae9fc19f57662c8218f35ad16b4
Rose Santos <rosesantos6891@yahoo.com>
2023-04-25 22:07:33 -0300

    falha no doc csv

--- relatório de ações na B3.py
+++ relatório de ações na B3.py
@@ -11,18 +11,18 @@ ABEV3.info()
 
 ABEV3.head()
 
-ABEV3.tail()
+ABEV3.tail(10)
 
 
-carteira_de_investimentos  = ['ABEV3.SA', 'ASAI3.SA', 'CRFB3.SA']
+carteira_de_investimentos = ['ABEV3.SA', 'ASAI3.SA', 'CRFB3.SA']
 novos_dados = pandas.DataFrame()
 for cdi in carteira_de_investimentos:
-    novos_dados[cdi] = web.DataReader(cdi, data_source='yahoo', start='2020-3-3')['Adj Close']
-    
+    novos_dados[cdi] = web.DataReader(
+        cdi, data_source='yahoo', start='2020-3-3')['Adj Close']
+
     novos_dados.tail()
-    
+
 novos_dados.to_csv(
     '/home/rose/portifólio/Faz relatórios de ações de empresas na B3/dados importados do doc em ods.csv')
-novos_dados1 = pd.read_csv(
+novos_dados2 = pandas.read_csv(
     '/home/rose/dados importados do doc em ods.csv')
-
